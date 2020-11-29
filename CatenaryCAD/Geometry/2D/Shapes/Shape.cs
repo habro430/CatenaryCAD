05f71ef73c360f6b204ea5a4ec1598e3fd1b9b01
habro430 <habro430@gmail.com>
2020-11-29 23:37:39 +0300

    работа с xml

--- CatenaryCAD/Geometry/2D/Shapes/Shape.cs
+++ CatenaryCAD/Geometry/2D/Shapes/Shape.cs
@@ -12,19 +12,19 @@ namespace CatenaryCAD.Geometry.Shapes
         public virtual IShape TransformBy(in Matrix2D m)
         {
             int count = Vertices.Length;
+
             for (int i = 0; i < count; i++)
                 Vertices[i] = Vertices[i].TransformBy(m);
 
             return this;
         }
 
-
         public virtual bool IsInside(in Point2D p)
         {
-            int npol = Vertices.Length;
+            int count = Vertices.Length;
 
             bool c = false;
-            for (int i = 0, j = npol - 1; i < npol; j = i++)
+            for (int i = 0, j = count - 1; i < count; j = i++)
             {
                 if ((((Vertices[i].Y <= p.Y) && (p.Y < Vertices[j].Y)) || ((Vertices[j].Y <= p.Y) && (p.Y < Vertices[i].Y))) &&
                   (((Vertices[j].Y - Vertices[i].Y) != 0) && (p.X > ((Vertices[j].X - Vertices[i].X) * (p.Y - Vertices[i].Y) / (Vertices[j].Y - Vertices[i].Y) + Vertices[i].X))))
--- CatenaryCAD/Geometry/3D/Meshes/IMesh.cs
+++ CatenaryCAD/Geometry/3D/Meshes/IMesh.cs
@@ -3,19 +3,29 @@
     public interface IMesh
     {
         /// <summary>
-        /// Массив вершин меша
+        /// Вершины описывающие меш.
         /// </summary>
+        /// <value>Массив <see cref="Point2D"/> с вершинами.</value>
         Point3D[] Vertices { get; }
+
         /// <summary>
         /// Массив нормалей меша
         /// </summary>
+        /// <value>Массив <see cref="Vector3D"/> с нормалями.</value>
         Vector3D[] Normals { get; }
+
         /// <summary>
-        /// Массив индексов вершин образующих полигоны и грани меша, 
-        /// где первый уровень массива - индекс полигона, а второй - индекс вершины 
+        /// Индексы полигонов и вершин описывающие меш.
         /// </summary>
+        /// <value>Массив индексов, где первый уровень массива - индекс полигона, а второй - индекс вершины .</value>
         int[][] Indices { get; }
 
+
+        /// <summary>
+        /// Трансформирует этот экземпляр <see cref="IMesh"/>, умножая все его вершины на <paramref name = "m" />.
+        /// </summary>
+        /// <param name="m">Матрица для умножения.</param>
+        /// <returns>Трансформированный экземпляр <see cref="IMesh"/>.</returns>
         IMesh TransformBy(in Matrix3D m);
 
     }
