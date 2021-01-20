7d66d6facbd4ff13e7d85cd0eea53a0269304f2c
habro430 <habro430@gmail.com>
2021-01-20 21:12:52 +0300

    начал работа над IPart

--- CatenaryCAD/Parts/IPart.cs
+++ CatenaryCAD/Parts/IPart.cs
@@ -4,12 +4,42 @@ using CatenaryCAD.Geometry.Meshes;
 
 namespace CatenaryCAD.Parts
 {
+    /// <summary>
+    /// Интерфейс, описывающий контракты для деталей моделей объектов контактной сети.
+    /// </summary>
     public interface IPart : IDirectionable<Vector3D>, IPositionable<Point3D>, ITransformable<Matrix3D, IPart>
     {
         /// <summary>
-        /// 3D геометрия для режима работы <see cref="OperationalMode.Layout"/>
+        /// Возвращает массив 3D геометрии с оносительными для детали координатами.
         /// </summary>
-        /// <returns>Массив объектов <see cref="IMesh"/></returns>
+        /// <value>
+        /// Массив с 3D геометрией .
+        /// </value>
         public IMesh[] Geometry { get; }
+
+        /// <summary>
+        /// Возвращает стоимость детали для рассчета сметы.
+        /// </summary>
+        /// <value>
+        /// Каталожая стоимость детали.
+        /// </value>
+        public decimal Price { get; }
+
+        /// <summary>
+        /// Возвращает массу детали.
+        /// </summary>
+        /// <value>
+        /// Масса детали в киллограммах.
+        /// </value>
+        public double Mass { get; }
+
+        /// <summary>
+        /// Возвращает центр массы с оносительными для детали координатами.
+        /// </summary>
+        /// <value>
+        /// Цетр массы детали.
+        /// </value>
+        public Point3D CenterOfMass { get; }
+
     }
 }
