297e417898f68eb594b4aff2dcca294300a678b3
jenseymo <jenseymo@jenseymo.com>
2021-05-06 22:16:37 +0300

    **

--- BasicBasements/Properties/Resources.Designer.cs
+++ BasicBasements/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BasicBasements.Properties {
+namespace BasicFoundations.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BasicBasements.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BasicBasements.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BasicFoundations.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
