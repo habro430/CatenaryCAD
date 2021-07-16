acb7c8cc09b35c20ab0d311927295500fa047490
jensaymoo <jensaymoo@jensaymoo.com>
2021-07-16 22:15:21 +0300

    добавил логику допустимых IMast в IAnchorTie

--- Catenary/Objects/Models/AnchorTie.cs
+++ Catenary/Objects/Models/AnchorTie.cs
@@ -10,6 +10,18 @@ namespace Catenary.Models
     [Serializable]
     public abstract class AnchorTie : Model, IAnchorTie
     {
+        /// <inheritdoc/>
+        /// <summary>
+        /// Типы допустимых моделей <see cref="IMast"/>, на которые может быть установлена данная модель <see cref="IAnchorTie"/>.
+        /// При наличии в массиве абстрактного класса, наследуемого от <see cref="IMast"/>, то допустимыми 
+        /// моделями являются все наследуемые от указанного абстрактного класса модели. 
+        /// При наличии класса определенной модели, наследуемого от <see cref="IMast"/>, то допустимой моделью 
+        /// являеться указанная модель и все наследуемые от указанного класса модели. 
+        /// </summary>
+        /// <remarks>По умолчанию возвращает все модели, наследуемые от <seealso cref="IMast"/>.</remarks>
+        public virtual Type[] AllowableMasts => new Type[] { typeof(IMast) };
+
+
         internal event Action<Type[]> AvailableAnchorTiesUpdated;
         internal void SetAvailableAnchorTies(Type[] anchorties)
         {
--- Catenary/Objects/Models/Interfaces/IAnchorTie.cs
+++ Catenary/Objects/Models/Interfaces/IAnchorTie.cs
@@ -1,10 +1,16 @@
-﻿namespace Catenary.Models
+﻿using System;
+
+namespace Catenary.Models
 {
     /// <summary>
     /// Интерфейс, описывающий контракты для модели анкерной оттяжки.
     /// </summary>
     public interface IAnchorTie : IModel
     {
-
+        /// <summary>
+        /// Типы допустимых моделей <see cref="IMast"/>, на которые может быть установлена данная модель <see cref="IAnchorTie"/>.
+        /// </summary>
+        /// <value>Модели, наследуемые от <seealso cref="IMast"/>.</value>
+        Type[] AllowableMasts{ get; }
     }
 }
